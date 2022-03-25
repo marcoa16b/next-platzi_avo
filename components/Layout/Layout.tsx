cc3fd252717f3023581f01cb907eb8b959a0ff76
marcoa16b <62965695+marcoa16b@users.noreply.github.com>
2022-03-25 17:38:40 -0600

    Agregando path alias

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '@components/Navbar/Navbar';
+// Agregamos a el archivo "tsconfig.json" la configuracion para 
+// poder usar "@components" y simplificar las rutas.
+
+// Es importante agregar las siguientes lineas para que funcione:
+// "baseUrl": "./", //        <-- para que reconozca la raiz
+// "paths": { //              <-- le indicamos las rutas
+//   "@database": [
+//     "database/db.ts"
+//   ],
+//   "@cors": ["cors-middleware.ts"],
+//   "@components/*": ["components/*"],
+// },
 
 const Layout: React.FC  = ({ children }) => {
   return (
