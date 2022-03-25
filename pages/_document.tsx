95a2802eff1bd3a71a382e27514ec71d05fce4c7
marcoa16b <62965695+marcoa16b@users.noreply.github.com>
2022-03-25 16:59:57 -0600

    extendiendo el Document

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+  render() {
+		return (
+      <Html>
+        <Head>
+          {/* favicon */}
+          {/* webfont */}
+          {/* stylessheet */}
+          {/* script/js */}
+        </Head>
+        <body className="my-body-class">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
